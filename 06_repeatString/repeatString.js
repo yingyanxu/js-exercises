b34a6c315728bb2739a7bb0cc0a34e8a806a9ac6
Iris Xu <xuiris142@gmail.com>
2025-07-20 12:03:57 -0600

    update 06_repeatString

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    if (num < 0) {
+        return "ERROR";
+    }
 
+    let ans = "";
+    for (let i = 0; i < num; i++){
+        ans += str;
+    }
+    return ans;
 };
 
 // Do not edit below this line
