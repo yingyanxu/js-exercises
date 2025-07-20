c56edf1c0ceb010dd8bb98f7f72d7df0228a8a5e
Iris Xu <xuiris142@gmail.com>
2025-07-20 13:11:45 -0600

    finish 07 tasks

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,4 +1,9 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let ans = "";
+    for (let i = str.length-1; i >= 0; i--){
+        ans += str[i];
+    }
+    return ans;
 
 };
 
